c0c26fb77941c9470239132cff7119475a9072e3
flazzarotto <fabrice.lazzarotto@gmail.com>
2020-11-13 19:35:04 +0100

    Modified input for clip.citation
    TextInput => TextInput.multiline.fullWidth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import { HydraAdmin } from "@api-platform/admin";
+import * as React from "react"
+import { HydraAdmin, EditGuesser, ResourceGuesser } from "@api-platform/admin"
+import {TextInput} from 'react-admin'
+import InputGuesser from "@api-platform/admin/lib/InputGuesser";
+import FieldGuesser from "@api-platform/admin/lib/FieldGuesser";
 
-// Replace with your own API entrypoint
-// For instance if https://example.com/api/books is the path to the collection of book resources, then the entrypoint is https://example.com/api
-export default () => (
-  <HydraAdmin entrypoint="http://kaamelott-backend.localhost/api" />
-);
+const clipEditItems = []
+for (let prop in {'name': false, 'url': false, 'duration': false, 'partOfEpisode': true, 'characters': true, 'tags': true}) {
+    clipEditItems.push(prop ? <InputGuesser key={prop} source={prop} /> : <FieldGuesser key={prop} source={prop} />)
+}
+
+const ClipEdit = props => (
+    <EditGuesser {...props}>
+        {clipEditItems}
+        <TextInput source={"citation"} key={"citation"} multiline fullWidth />
+    </EditGuesser>
+)
+
+const App = () => (
+  <HydraAdmin entrypoint="http://kaamelott-backend.localhost/api">
+      <ResourceGuesser name={"clips"} edit={ClipEdit}/>
+      <ResourceGuesser name={"episodes"}/>
+      <ResourceGuesser name={"tags"}/>
+      <ResourceGuesser name={"people"}/>
+  </HydraAdmin>
+)
+
+export default App
